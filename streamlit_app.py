8085a2e33f9514061fbe87feb6bae2f8d0e429cc
fl0wer4 <florecita7124@icloud.com>
2024-05-25 22:49:03 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '  
-    # st.writw (ingredients_string)
+    # st.write (ingredients_string)
         
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
@@ -44,8 +44,8 @@ if ingredients_list:
         fruityvice_response = requests.get("http://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
         
-        my_insert_stmt = """insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+        my_insert_stmt = """insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """','"""+ name_on_order + """')"""
 
     st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order') 
